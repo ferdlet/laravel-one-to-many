4c35d6cb7e3b8ba2ea2f97a7fdcfd6233a9656f5
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2022-02-17 18:58:55 +0100

    controller categories

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Post;
 use Illuminate\Support\Str;
+use App\Category;
 
 
 class PostController extends Controller
@@ -34,8 +35,9 @@ class PostController extends Controller
      */
     public function create()
     {
-        return view("admin.posts.create");
+        $categories = Category::all();
         
+        return view("admin.posts.create", compact("categories"));
     }
 
     /**
--- app/Post.php
+++ app/Post.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    public function category()
-    {
+    public function category() {
         return $this->belongsTo("App\Category");
     }
 }
